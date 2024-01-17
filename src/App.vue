7e6dfa5aeb7916b194b08d3a3865721dfd254a69
Noemi-Guarino <noemi.guarino.1996@gmail.com>
2024-01-17 15:33:21 +0100

    compleate

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+
+<script>
+// importazione componente apptitle
+
+import AppTitle from './components/AppTitle.vue';
+export default {
+  data() {
+    return { 
+
+    }
+  },
+  // dichiarazione del componente all'interno dell'ogettoù
+  components:{
+    AppTitle
+  }
+};
+</script>
+
+<template>
+  <div>
+    <!-- utilizzo del componente -->
+    <AppTitle/>
+  </div>
+</template>
+
+<style scoped>
+</style>
